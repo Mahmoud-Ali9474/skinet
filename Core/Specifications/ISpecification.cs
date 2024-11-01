fd59bed98893aa16252de5cb5e9ea330a10f3f0d
Mahmoud Ali <Mahmoud.Ali9474@gmail.com>
2024-11-01 17:47:29 +0200

    add specification pattern

---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Specifications;
+
+public interface ISpecification<TEntity> where TEntity : BaseEntity
+{
+    Expression<Func<TEntity, bool>> Criteria { get; }
+    List<Expression<Func<TEntity, object>>> Includes { get; }
+}
